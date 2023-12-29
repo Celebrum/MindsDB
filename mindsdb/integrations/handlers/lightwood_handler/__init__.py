ca45162a973a52f3210ce685e40f09ebc36b2ca9
Max Stepanov <stpmax@yandex.ru>
2023-12-29 14:36:55 +0300

    debug

--- mindsdb/integrations/handlers/lightwood_handler/__init__.py
+++ mindsdb/integrations/handlers/lightwood_handler/__init__.py
@@ -3,11 +3,16 @@ from mindsdb.integrations.libs.const import HANDLER_TYPE
 from .__about__ import __description__ as description
 from .__about__ import __version__ as version
 
-try:
-    from .lightwood_handler import LightwoodHandler as Handler
+from mindsdb.utilities import log
+logger = log.getLogger(__name__)
 
+try:
+    logger.error('LIGHTWOOD importing')
+    from .lightwood_handler import LightwoodHandler as Handler
+    logger.error('LIGHTWOOD imported')
     import_error = None
 except Exception as e:
+    logger.error(f'LIGHTWOOD error {e}')
     print(f'ERROR import lightwood handler: {e}')  # noqa
     Handler = None
     import_error = e
